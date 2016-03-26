774d6ec2d439cf0f692b131ae0a611aa57c1c03c
Robert <0890289@hr.nl>
2016-03-26 15:50:16 +0100

    Header recognition problem

--- PoulePopulator/src/FileChoosing/csvFileHandler.java
+++ PoulePopulator/src/FileChoosing/csvFileHandler.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package FileChoosing;
 
 import java.io.BufferedReader;
@@ -16,6 +11,30 @@ import Algorithms.SortingAlgorithm;
 /**
  *
  * @author Kraaijeveld
+ * 
+ * 
+ * Actual CSV:
+ * Skill;Name;School;
+ * 0     1    2
+ * skillv;namev;schoolv;
+ * skillv;namev;schoolv;
+ * skillv;namev;schoolv;
+ * skillv;namev;schoolv;
+ * skillv;namev;schoolv;
+ * 
+ * ArrayList of given names
+ * IE: 
+ * NameColumn = X, SkillColumn = Y, SchoolColumn = Z
+ * 0                  1                    2
+ * 
+ * 
+ * We have to count where we first find the given column
+ * when looping through the CSV. 
+ * 
+ * We loop through the csv, keeping track with a counter.
+ * If we find a value, for instance, 'Skill' that is present
+ * in the givenRowNames list, we set that columns count-variable
+ * to be the current value of the counter.
  */
 
 public class csvFileHandler 
@@ -23,11 +42,7 @@ public class csvFileHandler
     //Allow the user to specify the columns:
     //Count each header column name and put them in an array
     //Compare each of these to the given columns
-    //Only print the value if their position == 
-    
-    //What this class should do:
-    //- If so, store their relative positions in a data structure
-    //- And echo out (later we will store them) all values at those positions
+    //Only print the value if their position % their appropriate headers' position == 0
     
     private File selectedFile;
     private ArrayList<String> givenHeaderColumnNames;
@@ -59,20 +74,18 @@ public class csvFileHandler
         selectedFile = f;
     }
     
-    //todo
-    private int[] getHeaderColumnPositions() throws Exception
+    private void getHeaderColumnPositions() throws Exception
     {
         String pathToFile = selectedFile.getAbsolutePath();
         BufferedReader reader = null;
         String currentLine = "";
         String delimiter = ";";
-        
         reader = new BufferedReader(new FileReader(pathToFile));
+
+        int positionCounter = 0;
         
         if((currentLine = reader.readLine()) != null)
         {
-            int positionCounter = 0;
-            int[] positionReturnList;
             String[] headerColumn = currentLine.split(delimiter);
             Arrays.asList(headerColumn);
             
@@ -81,13 +94,15 @@ public class csvFileHandler
                 positionCounter++;
                 if(givenHeaderColumnNames.contains(s))
                 {
-                    
+                    //We know that this header is contained in the givenheaders,
+                    //but we dont know which header it is, so we dont know which
+                    //headerPosition variable deserves to get the positioncounter assigned to it
                 }
             }
         }
+        return positionReturnList;
     }
-    
-    //Take the given headercolumnnames, check if they are existant in the header columns of the file
+
     private boolean givenHeaderColumnNamesExist() throws Exception
     {
         String pathToFile = selectedFile.getAbsolutePath();
@@ -124,9 +139,10 @@ public class csvFileHandler
         private int skillHeaderPosition;
         private int schoolHeaderPosition;
         
-        public CsvFileMetaData(File f)
+        public CsvFileMetaData(File f, ArrayList<Integer> listOfHeaderPositions)
         {
-            
+            selectedFile = f;
+            //for(int headerPosition : listOfHeaderPositions)
         }
     }
     
